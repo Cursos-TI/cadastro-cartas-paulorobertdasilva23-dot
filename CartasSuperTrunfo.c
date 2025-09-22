fa75de1d0023f7f458214dd4818fe007f29c7329
paulorobertdasilva23-dot <paulorobertdasilva23@gmail.com>
2025-09-22 14:41:35 -0300

    variáveis delaradas CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int numero_do_carta;
+  char letra_do_estado;
+  char codigo_da_carta[3]; 
+  char nome_da_cidade[50];
+  int populacao;
+  float area_do_estado;
+  float PIB;
+  float pontos_turisticos;
+  
+Código: A01
 
+Nome da Cidade: São Paulo
+
+População: 12325000
+
+Área: 1521.11 km²
+
+PIB: 699.28 bilhões de reais
+
+Número de Pontos Turísticos: 50
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
