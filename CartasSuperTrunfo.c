4eba7257ca8408db10f6e4db452bc333b81fb036
paulorobertdasilva23-dot <paulorobertdasilva23@gmail.com>
2025-09-22 19:56:32 +0000

    teste dos printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,42 +11,42 @@ int main() {
   char codigo_da_carta[3]; 
   char nome_da_cidade[50];
   int populacao;
-  float area_do_estado;
+  float area_da_cidade;
   float PIB;
   float pontos_turisticos;
   
   // Área para entrada de dados
     
   //Introdução do usuário ao jogo
-   printf("Óla bem-vindo ao Super Trunfo, nosso jogo de cartas, aqui você irá cadastrar cartas de cidades brasileiras com informções desde seu o nome até seu PIB se divirta!! \n")
+   printf("Óla bem-vindo ao Super Trunfo, nosso jogo de cartas, aqui você irá cadastrar cartas de cidades brasileiras com informções desde seu o nome até seu PIB se divirta!! \n");
   
   //Armazenando o número da carta informado pelo usuário  
-    printf("Insira o número da carta");
+    printf("Insira o número da carta \n");
     scanf("%d", &numero_do_carta);
 
   //Armazenando o número da carta informado pelo usuário   
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira a letra do Estado onde a cidade se encontra \n");
+    scanf("%c", &letra_do_estado);
   
   //Armazenando o número da carta informado pelo usuário
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira o código da carta seguindo o exemplo a seguir. Ex: A01 \n");
+    scanf("%d", &codigo_da_carta);
   
   //Armazenando o número da carta informado pelo usuário
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira a população da cidade \n");
+    scanf("%d", &populacao);
   
   //Armazenando o número da carta informado pelo usuário
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira a área da cidade \n");
+    scanf("%d", &area_da_cidade);
   
   //Armazenando o número da carta informado pelo usuário
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira o PIB da cidade \n");
+    scanf("%d", &PIB);
   
   //Armazenando o número da carta informado pelo usuário
-    printf("Insira o número da carta");
-    scanf("%d", &numero_do_carta);
+    printf("Insira o um ou mais pontos turísticos da cidade \n");
+    scanf("%d", &pontos_turisticos);
   
 // Área para exibição dos dados da cidade
 
